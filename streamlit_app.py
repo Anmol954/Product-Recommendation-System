e0b1edec8104694fa74383a8d1571040a8ceeb40
SANDWICH <anmolmadhav2004@gmail.com>
2025-06-27 10:59:15 +0530

    All set

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import MinMaxScaler
-from scraper import scrape_amazon_products  # your scraper function
+from scrapper import scrape_amazon_products  # your scraper function
 
 # Set page config
 st.set_page_config(page_title="🛍️ Live Product Recommender", layout="wide")
