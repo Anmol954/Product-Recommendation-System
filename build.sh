a2a4c914ddc92bc9e49463ba4366de48a93a82c4
SANDWICH <anmolmadhav2004@gmail.com>
2025-06-27 13:05:18 +0530

    final

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update
-apt-get install -y wget unzip curl gnupg
+sudo apt-get update
+sudo apt-get install -y wget unzip curl gnupg
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
